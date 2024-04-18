cba861e1fc16c3bbee7e8a7d2855483e0366dc4c
Raj Aryan <rajxryn@gmail.com>
2024-04-18 15:50:21 +0530

    analytics setup
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
           {children}
         </main>
       </body>
+      <GoogleAnalytics gaId="G-12ZNJQ44QV" />
     </html>
   );
 }
